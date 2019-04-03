bdb867d5b29127cccfab7a2a9e54b5e972dc067d
azheng8072 <43213098+azheng8072@users.noreply.github.com>
2019-04-03 11:39:24 -0400

    Update SimpleMerge.java
    update

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -5,8 +5,37 @@
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+    public static int[] simpleMerge(int[] array1, int[] array2) {
+        int[] array3 = new int[array1.length+array2.length];
 
+        for (int i = 0; i < array1.length; i++) {
+            array3[i] = array1[i];
+            int x = 0;
+            for (int j = array1.length; j < array3.length; j++) {
+                array3[j] = array2[x];
+                x++;
+            }
+        }
+        bubbleSort(array3);
+        return array3;
+    }
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void bubbleSort(int[] array) {
+        int swaps = 1;
+        while (swaps!=0) {
+            swaps = 0;
+            for (int i = 0; i < array.length-1; i++) {
+                if (array[i] > array[i+1]) {
+                    swap(array, i, i+1);
+                    swaps++;
+                }
+            }
+        }
     }
 
 }
